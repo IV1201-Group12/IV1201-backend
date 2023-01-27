59631ddda5670ed6767e2e1ffd372b7b711fefd9
EdvAlvaKTH <ealvaeus@kth.se>
2023-01-27 18:41:32 +0100

    feat: login endpoint and jwt functionality; bcrypt password functionality

---
+++ src/app/utils/jwt.js
@@ -0,0 +1,20 @@
+const jwt = require('jsonwebtoken');
+
+const secret = 'TEMPORARYSECRETASDASDSDSADSADSADASDASDAS';
+
+module.exports = {
+  generateToken: (user) => {
+    return jwt.sign({ id: user.id, username: user.username }, secret, {
+      expiresIn: '1h',
+    });
+  },
+  verifyToken: (token) => {
+    try {
+      const decodedToken = jwt.verify(token, secret);
+      return decodedToken;
+    } catch (err) {
+      //todo
+      throw new Error('Bad token');
+    }
+  },
+};
