a6e226f6542086c2a26c61c7d69d5f312e18e02c
Abhinav Kalra <abhinavkalra2002@gmail.com>
2023-01-30 15:51:11 +0100

    refactor: change userDAO to RecruitmentDAO

---
+++ src/app/integration/RecruitmentDAO.js
@@ -0,0 +1,40 @@
+const { generateHashSync } = require('../utils/bcrypt');
+const { Sequelize } = require('sequelize');
+
+const users = [
+  { username: 'edvin', password: generateHashSync('pass') },
+  { username: 'edvin2', password: generateHashSync('pass2') },
+];
+
+const sequelize = new Sequelize(
+  'recruitment_application',
+  'postgres',
+  'postgres',
+  {
+    host: 'localhost',
+    dialect: 'postgres',
+  },
+);
+
+sequelize.sync();
+
+(async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+})();
+
+const queryInterface = sequelize.getQueryInterface();
+
+module.exports = {
+  getExistingUser: (username) => {
+    return users.find((user) => user.username === username);
+  },
+
+  createApplicant: async (applicant) => {
+    queryInterface.bulkInsert('person', [applicant]);
+  },
+};
