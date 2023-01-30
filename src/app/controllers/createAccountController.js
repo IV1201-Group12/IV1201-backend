a7ab6b6b0d70baf6346e5279d6eb70fc7bccccd1
Abhinav Kalra <abhinavkalra2002@gmail.com>
2023-01-30 22:07:53 +0100

    feat: add validation

--- src/app/controllers/createAccountController.js
+++ src/app/controllers/createAccountController.js
@@ -1,12 +1,21 @@
 const RecruitmentDAO = require('../integration/RecruitmentDAO');
+const Validators = require('../utils/validators');
 
 module.exports = {
   createApplicant: async (req, res, next) => {
     try {
+      if (!Validators.isString(req.body.name))
+        throw new Error('Name is not valid');
+      else if (!Validators.isString(req.body.surname))
+        throw new Error('Surname is not valid');
+      else if (!Validators.isValidEmail(req.body.email))
+        throw new Error('Email is not valid');
+      else if (!Validators.isValidPnr(req.body.pnr))
+        throw new Error('Person number is not valid');
       await RecruitmentDAO.createApplicant(req.body);
       res.status(201).send();
     } catch (err) {
-      console.log(err);
+      throw new Error(err.message);
     }
   },
 };
