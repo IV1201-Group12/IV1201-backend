abde150cf10a8588d73f6e1a45986a876c226475
Jakob Olsson <53495529+maskenjesper@users.noreply.github.com>
2023-01-31 23:56:19 +0100

    chore: remove test log

--- src/app/controllers/applicationController.js
+++ src/app/controllers/applicationController.js
@@ -4,7 +4,6 @@ module.exports = {
   getAllApplications: async (req, res) => {
     try {
       const applications = await db.findAllApplications();
-      console.log(applications);
       res.json(applications);
     } catch (err) {
       res.status(500).send(err);
