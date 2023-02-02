18bdd00d76774095390b1f196b5f2a037325b960
Jakob Olsson <53495529+maskenjesper@users.noreply.github.com>
2023-02-02 21:42:41 +0100

    refactor: combined authentication related endpoints

--- src/app/controllers/authController.js
+++ src/app/controllers/authController.js
@@ -1,8 +1,25 @@
 const RecruitmentDAO = require('../integration/RecruitmentDAO');
+const Validators = require('../utils/validators');
 const { comparePassword } = require('../utils/bcrypt');
 const { generateToken } = require('../utils/jwt');
 
 module.exports = {
+  createApplicant: async (req, res, next) => {
+    try {
+      if (!Validators.isString(req.body.name))
+        throw new Error('Name is not valid');
+      else if (!Validators.isString(req.body.surname))
+        throw new Error('Surname is not valid');
+      else if (!Validators.isValidEmail(req.body.email))
+        throw new Error('Email is not valid');
+      else if (!Validators.isValidPnr(req.body.pnr))
+        throw new Error('Person number is not valid');
+      await RecruitmentDAO.createApplicant(req.body);
+      res.status(201).send();
+    } catch (err) {
+      throw new Error(err.message);
+    }
+  },
   login: async (req, res) => {
     const { username, password } = req.body;
     const existingUser = RecruitmentDAO.getExistingUser(username);
--- src/app/routes/createAccountRoute.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const createAccountController = require('../controllers/createAccountController');
-
-router.post('/', createAccountController.createApplicant);
-
-module.exports = router;
