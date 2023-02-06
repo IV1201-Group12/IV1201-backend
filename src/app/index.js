3d381cf01594fb4ee53742ea2d932b1a8676500f
EdvAlvaKTH <ealvaeus@kth.se>
2023-02-06 19:32:25 +0100

    fix: CORS issues

--- src/app/index.js
+++ src/app/index.js
@@ -7,13 +7,22 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
-const corsOptions = {
-  origin: 'http://127.0.0.1:5173',
-  credentials: true,
-};
+// const corsOptions = {
+//   origin: ['http://127.0.0.1:5173', 'http://localhost:5173'],
+//   credentials: true,
+// };
 
-app.use(cors(corsOptions));
-
+// app.use(cors(corsOptions));
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+  res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:5173');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, X-PINGOTHER, ACCESSTOKEN, Access-Control-Allow-Origin',
+  );
+  next();
+});
 app.get('/', (req, res) => {
   return res.send('helloo');
 });
--- src/app/routes/authRoute.js
+++ src/app/routes/authRoute.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
+const { authorizeRequest } = require('../middleware/authorization');
 
 router.post('/createAccount', authController.createApplicant);
 router.post('/login', authController.login);
-
+router.get('/test', authorizeRequest, (req, res) => {
+  res.status(200).send('hej');
+});
 module.exports = router;
