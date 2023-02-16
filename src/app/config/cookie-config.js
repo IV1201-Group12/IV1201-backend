05af1d9346b5769288015f4231318d5ab6d04cab
EdvAlvaKTH <ealvaeus@kth.se>
2023-02-16 16:01:46 +0100

    refactor: cookie and cors settings for deployment

---
+++ src/app/config/cookie-config.js
@@ -0,0 +1,16 @@
+export const cookieConfig = () => {
+  if (process.NODE.env === 'production') {
+    return {
+      httpOnly: true,
+      sameSite: 'None',
+      secure: true,
+      maxAge: 60 * 60 * 1000, //1h
+    };
+  } else {
+    return {
+      httpOnly: true,
+      sameSite: 'Strict',
+      maxAge: 60 * 60 * 1000, //1h
+    };
+  }
+};
---
+++ src/app/config/cors-config.js
@@ -0,0 +1,13 @@
+export const corsOptions = () => {
+  if (process.NODE.env === 'production') {
+    return {
+      origin: ['https://iv1201-frontend.onrender.com'],
+      credentials: true,
+    };
+  } else {
+    return {
+      origin: ['http://127.0.0.1:5173', 'http://localhost:5173'],
+      credentials: true,
+    };
+  }
+};
