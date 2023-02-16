b5ba539e36ed24dfe4f31f31255324a21e1f46a7
EdvAlvaKTH <ealvaeus@kth.se>
2023-02-16 16:03:28 +0100

    refactor: cookie and cors settings for deployment

--- src/app/config/cookie-config.js
+++ src/app/config/cookie-config.js
@@ -1,16 +1,18 @@
-export const cookieConfig = () => {
-  if (process.NODE.env === 'production') {
-    return {
-      httpOnly: true,
-      sameSite: 'None',
-      secure: true,
-      maxAge: 60 * 60 * 1000, //1h
-    };
-  } else {
-    return {
-      httpOnly: true,
-      sameSite: 'Strict',
-      maxAge: 60 * 60 * 1000, //1h
-    };
-  }
+module.exports = {
+  cookieConfig: () => {
+    if (process.NODE.env === 'production') {
+      return {
+        httpOnly: true,
+        sameSite: 'None',
+        secure: true,
+        maxAge: 60 * 60 * 1000, //1h
+      };
+    } else {
+      return {
+        httpOnly: true,
+        sameSite: 'Strict',
+        maxAge: 60 * 60 * 1000, //1h
+      };
+    }
+  },
 };
--- src/app/config/cors-config.js
+++ src/app/config/cors-config.js
@@ -1,13 +1,15 @@
-export const corsOptions = () => {
-  if (process.NODE.env === 'production') {
-    return {
-      origin: ['https://iv1201-frontend.onrender.com'],
-      credentials: true,
-    };
-  } else {
-    return {
-      origin: ['http://127.0.0.1:5173', 'http://localhost:5173'],
-      credentials: true,
-    };
-  }
+module.exports = {
+  corsOptions: () => {
+    if (process.NODE.env === 'production') {
+      return {
+        origin: ['https://iv1201-frontend.onrender.com'],
+        credentials: true,
+      };
+    } else {
+      return {
+        origin: ['http://127.0.0.1:5173', 'http://localhost:5173'],
+        credentials: true,
+      };
+    }
+  },
 };
