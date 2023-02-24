91ed1b81a59021d60e357a3dad67431183433867
Abhinav Kalra <abhinavkalra2002@gmail.com>
2023-02-24 12:55:10 +0100

    chore: add comments and delete login test file

--- src/app/controllers/authController.js
+++ src/app/controllers/authController.js
@@ -9,21 +9,33 @@ const database = require('../integration/database');
 const ValidationError = require('sequelize');
 
 module.exports = {
+  /**
+   * An asynchronous function that registers a new user.
+   * Sets the role of the new user to 'applicant'.
+   * @param {*} req The HTTP request object.
+   * @param {*} res The HTTP response object.
+   */
   register: async (req, res) => {
-
-    return database.sequelize.transaction(async (t) => {
-      req.body.role = 'applicant';
-      try {
-        await userRepository.createUser(req.body);
-      } catch (err) {
-        if (err instanceof ValidationError) {
-          res.status(409).send(err.errors[0].message);
-        } else {
-          res.status(400).send('Server error');
-        }
+    req.body.role = 'applicant';
+    try {
+      await userRepository.createUser(req.body);
+    } catch (err) {
+      if (err instanceof ValidationError) {
+        res.status(409).send(err.errors[0].message);
+      } else {
+        res.status(400).send('Server error');
+      }
       res.status(201).send();
-    });
+    }
   },
+  /**
+   * An asynchronous function that logs a user in.
+   * If the user does not exist, returns a 401 Unauthorized response.
+   * Generates a token for the user using the generateToken function.
+   * @param {*} req The HTTP request object.
+   * @param {*} res The HTTP response object.
+   * @returns 200 OK response with the user's username and role in JSON format.
+   */
   login: async (req, res) => {
     return database.sequelize.transaction(async (t) => {
       const { username, password } = req.body;
@@ -42,6 +54,11 @@ module.exports = {
         .json({ username: existingUser.username, role: existingUser.role });
     });
   },
+  /**
+   * Logs the user out by setting the access token cookie to 'none'.
+   * @param {*} req The HTTP request object.
+   * @param {*} res The HTTP response object.
+   */
   logout: async (req, res) => {
     res.cookie('ACCESSTOKEN', 'none', cookieConfigLogout());
     res.status(200).json({ message: 'Logged out' });
