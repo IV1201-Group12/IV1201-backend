613c6e4cf5a96b6d00d553ec07f7d231b6a4e11a
EdvAlvaKTH <ealvaeus@kth.se>
2023-03-09 16:18:13 +0100

    chore: comments

--- src/app/routes/userRoute.js
+++ src/app/routes/userRoute.js
@@ -1,3 +1,10 @@
+/**
+ * This module sets up the routes for user related endpoints.
+ * More granular endpoint-specific middleware can be added in this file.
+ *
+ * This module is what connects the express framework to the user controller.
+ */
+
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
